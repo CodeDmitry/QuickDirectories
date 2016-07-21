1f9ba33deba3196eaa72ea70b573e6e17324ed74
root <root@localhost.localdomain>
2016-07-21 17:14:44 -0400

    Greatly simplified installation process.

---
+++ preset/apps/startup/setup.sh
@@ -0,0 +1,7 @@
+# runs all setups
+
+user=`id -u -n`
+source /$user/QuickDirs/apps/startup/min-setup.sh
+source /$user/QuickDirs/apps/startup/core-app-setup.sh
+source /$user/QuickDirs/apps/startup/my-app-setup.sh
+
