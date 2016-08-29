afee36c502ab0fc6eb670967bd9130f7a7141cc0
dmitrymakhnin <dimamakhnin@gmail.com>
2016-08-29 02:48:36 -0400

    fixed problem where I was not using HOME variable, causing the project to not run on some profiles that did not have home at /username/

--- preset/apps/startup/setup.sh
+++ preset/apps/startup/setup.sh
@@ -1,7 +1,13 @@
 # runs all setups
 
-user=`id -u -n`
-source /$user/QuickDirs/apps/startup/min-setup.sh
-source /$user/QuickDirs/apps/startup/core-app-setup.sh
-source /$user/QuickDirs/apps/startup/my-app-setup.sh
+
+if [ -n $HOME ]; then
+    source $HOME/QuickDirs/apps/startup/min-setup.sh
+    source $HOME/QuickDirs/apps/startup/core-app-setup.sh
+    source $HOME/QuickDirs/apps/startup/my-app-setup.sh
+else
+    echo "setup.sh> Please set your HOME variable.";
+    echo "setup.sh> shutting down."
+    exit;
+fi
 
