cf66ebb5fa09c6e84d78e29fe61d3373800de79f
CodeDmitry <3159365+CodeDmitry@users.noreply.github.com>
2022-08-21 19:16:16 -0400

    Update setup.sh
    Made responsibility of the file more explicit.

--- preset/apps/startup/setup.sh
+++ preset/apps/startup/setup.sh
@@ -1,13 +1,22 @@
-# runs all setups
-
+#!/bin/bash
+# | @file setup.sh
+# | Responsible for running all the setup scripts in 
+# |     the correct order.
+# | The contents of this file can be cut and pasted
+# |     directly into bashrc, or it can be sourced
+# |     as `$HOME/QuickDirs/apps/startup/setup.sh`.
 
 if [ -n $HOME ]; then
+    # | Important: min-setup.sh must be sourced first.
+    # |     The other files can be sourced in any order.
     source $HOME/QuickDirs/apps/startup/min-setup.sh
     source $HOME/QuickDirs/apps/startup/core-app-setup.sh
     source $HOME/QuickDirs/apps/startup/my-app-setup.sh
 else
+    # | We cannot reliably do anything without knowing 
+    # |     where the user's home directory is.
     echo "setup.sh> Please set your HOME variable.";
-    echo "setup.sh> shutting down."
+    echo "setup.sh> Nothing has been changed.";
     exit;
 fi
 
